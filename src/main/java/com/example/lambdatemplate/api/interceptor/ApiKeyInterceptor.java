126c974b7512467239af7a9aaca6bf3187e39f16
Adam Maas <adamjm1220@gmail.com>
2024-10-04 16:10:29 -0400

    Updated_Base_Template

--- src/main/java/com/example/lambdatemplate/api/interceptor/ApiKeyInterceptor.java
+++ src/main/java/com/example/lambdatemplate/api/interceptor/ApiKeyInterceptor.java
@@ -21,7 +21,7 @@ public class ApiKeyInterceptor implements ClientHttpRequestInterceptor{
     @SuppressWarnings("null")
     @Override
     public ClientHttpResponse intercept(HttpRequest request, byte[] body, ClientHttpRequestExecution execution) throws IOException {
-        request.getHeaders().set("X-Api-Key", apikey);
+        request.getHeaders().set("<Api Key Name>", apikey);
         return execution.execute(request, body);
     }
 }
