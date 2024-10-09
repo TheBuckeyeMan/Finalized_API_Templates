ed58d3a2f59766c2bd03d3ba1c01583909426c0d
Adam Maas <adamjm1220@gmail.com>
2024-10-09 18:04:18 -0400

    Python Lambda Template

---
+++ ScriptFile.py
@@ -0,0 +1,10 @@
+
+
+def handler(event, context):
+    #Trigger your code here
+    return Execute()
+
+#Define your Script here
+def Execute():
+    message = "Hello for lambda"
+    return message
